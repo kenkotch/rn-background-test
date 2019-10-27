b880ce18c0303a64f8d6fa8673bef227e05f16d6
Ken Kotch <hello@ken.uno>
2019-10-27 09:47:01 -0600

    unmounts background webview after code completes

--- src/App.js
+++ src/App.js
@@ -7,25 +7,34 @@ import {
 import BackgroundWebView from './BackgroundWebView';
 
 class App extends React.PureComponent {
+	state = {
+		runCode: true
+	}
+
 	code = () => {
-		for (let i = 0;; i++) {
+		for (let i = 0; i < 999999999; i++) {
 			if (i % 100000 === 0) {
-				console.log(Math.random() * 9999 / 7)
+				console.log(i, Math.random() * 9999 / 7)
 			}
-			if (i === 1000001) { i = 0 }
 		}
+		setTimeout(() => {
+			this.setState({ runCode: false })
+		}, 0);
 	}
 	render() {
+		const { runCode } = this.state;
 		return (
 			<View style={{ marginLeft: 15, marginBottom: 50 }}>
+				{ runCode &&
+					<BackgroundWebView
+						code={ this.code }
+					/>
+				}
 				<Text style={{ marginTop: 25 }}>Hello World</Text>
 				<FlatList
 					data={ [...Array(230).keys()] }
 					keyExtractor={ (item, index) => index.toString() }
 					renderItem={ (item) => <Text>{ item.item }</Text> }
-				/>
-				<BackgroundWebView
-					code={ this.code }
 				/>
 			</View>
 		)
--- src/BackgroundWebView.js
+++ src/BackgroundWebView.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import { WebView } from 'react-native-webview';
 export default class BackgroundWebView extends Component {
 	componentDidMount() {
+		console.info('bg mounted >>>');
 		const { code } = this.props;
 		setTimeout(() => {
 			this.runJSInBackground(code)
 		}, 1000);
+	}
+
+	componentWillUnmount() {
+		console.info('bg unmounted >>>');
 	}
    
 	render() {
